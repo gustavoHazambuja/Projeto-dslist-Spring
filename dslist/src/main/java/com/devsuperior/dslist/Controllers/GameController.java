626e64b598bda141408be3ac68390bad946b1511
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-21 19:59:44 -0300

    dominio

--- dslist/src/main/java/com/devsuperior/dslist/Controllers/GameController.java
+++ dslist/src/main/java/com/devsuperior/dslist/Controllers/GameController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.repositories.GameService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
